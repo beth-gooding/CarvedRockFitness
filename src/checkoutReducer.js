5de80dbd0d0d4ed5685b51b418f513884a5d8e26
Beth Gooding <beth.gooding@hotmail.com>
2021-09-09 16:38:49 +0100

    Adds more cases to the checkoutReducer

--- src/checkoutReducer.js
+++ src/checkoutReducer.js
@@ -2,7 +2,13 @@ export default function checkoutReducer(billingAddress, action) {
     switch (action.type) {
         case "copy":
             const { city, country } = action;
-            console.log("Copy the shipping address to billing address")
+            console.log("Copy the shipping address to billing address");
+            break
+        case "updateShippingAddress":
+            console.log("Shipping addresses is being updated");
+            break
+        case "updateBillingAddress":
+            console.log("The billing address is being updated");
             break
         default:
             throw new Error("Unknown action: " + action.type);
